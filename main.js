3ca8454e1fc697ca5acf1a8625582d314b9d997f
heraldofortuna <heraldofortuna@gmail.com>
2021-12-06 13:39:08 -0500

    El algoritmo principal está completo pero falta refactorizar

---
+++ main.js
@@ -0,0 +1,38 @@
+const balancedParentheses = (str) => {
+  const stack = [];
+
+  for (let idx = 0; idx < str.length; idx += 1) {
+    if (str[idx] === "(") {
+      if (str[idx - 1] !== ":") {
+        stack.push(str[idx]);
+      }
+    }
+    if (str[idx] === ")") {
+      if (str[idx - 1] === ":") {
+        if (str[idx - 2] === "(") {
+          // console.log("No es una carita");
+          stack.pop();
+        } else {
+          // console.log("Encontré una carita");
+        }
+      } else {
+        if (stack.length === 0) {
+          return false;
+        }
+
+        stack.pop();
+      }
+    }
+  }
+
+  return stack.length === 0 ? "Balanceado" : "Desbalanceado";
+};
+
+console.log("a.", balancedParentheses("hola")); // Balanceado
+console.log("b.", balancedParentheses("(hola)")); // Balanceado
+console.log("c.", balancedParentheses("(()")); // Desbalanceado
+console.log("d.", balancedParentheses("(:)")); // Balanceado
+console.log("e.", balancedParentheses("no voy (:()")); // Balanceado
+console.log("f.", balancedParentheses("hoy pm: fiesta :):)")); // Balanceado
+console.log("g.", balancedParentheses(":((")); // Desbalanceado
+console.log("h.", balancedParentheses("a (b (c (d) c) b) a :)")); // Balanceado
